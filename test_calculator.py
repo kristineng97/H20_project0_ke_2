a9d7436d41eb4ae7949d8c606e1486484ec674b1
Kristin Eng <kristineng@eduroam-193-157-227-135.wlan.uio.no>
2020-08-27 09:43:23 +0200

    Ferdig med Exercise 2

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,13 @@
 import pytest
 import calculator
 
-def test_add():
+def test_add_exercise_1():
     x = 1
     y = 2
     assert calculator.add(x,y) == 3
+
+def test_add_exercise_2():
+    x = 0.1
+    y = 0.2
+    tol = 1e-10
+    assert (calculator.add(x,y) - 0.3) < tol
