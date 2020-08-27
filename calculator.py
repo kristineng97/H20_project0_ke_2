30d8a7269f541db14494126d230f461ed3c4e8e2
Kristin Eng <kristineng@Kristins-MacBook-Air.local>
2020-08-27 13:14:26 +0200

    Ferdig med Exercise 4

--- calculator.py
+++ calculator.py
@@ -1,2 +1,48 @@
 def add(x,y):
     return (x+y)
+
+"""
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        i = 2
+        last_product = 1
+        while i <= n:
+            product = i * last_product
+            last_product = product
+            i += 1
+        return product
+"""
+
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        product = 1
+        for i in range(1, n+1):
+            product *= i
+        return product
+
+def sin(x,N):
+    sum = 0
+    for n in range(N+1):
+        sum += ((-1)**n * x**(2*n + 1))/(factorial(2*n + 1))
+    return sum
+
+def divide(x,y):
+    return (x/y)
+
+def multiply(x,y):
+    return x*y
+
+def cos(x,N):
+    sum = 0
+    for n in range(N+1):
+        sum += ((-1)**n * x**(2*n))/(factorial(2*n))
+    return sum
+    
--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,7 @@
 #from calculator import *
 import pytest
 import calculator
+import math
 
 def test_add_exercise_1():
     x = 1
@@ -11,9 +12,36 @@ def test_add_exercise_2():
     x = 0.1
     y = 0.2
     tol = 1e-10
-    assert (calculator.add(x,y) - 0.3) < tol
+    assert abs(calculator.add(x,y) - 0.3) < tol
 
 def test_add_exercise_3():
     x = "Hello "
     y = "World"
     assert calculator.add(x,y) == "Hello World"
+
+def test_factorial_exercise_4():
+    n = 3
+    tol = 1e-10
+    assert abs(calculator.factorial(n) - math.factorial(n)) < tol
+
+def test_sin_exercise_4():
+    x = 2*math.pi
+    N = 30
+    tol = 1e-10
+    assert abs(calculator.sin(x,N) - math.sin(x)) < tol
+
+def test_divide_exercise_4():
+    x = 4
+    y = 2
+    assert calculator.divide(x,y) == 2
+
+def test_multiply_exercise_4():
+    x = 3
+    y = 3
+    assert calculator.multiply(x,y) == 9
+
+def test_cos_exercise_4():
+    x = 2*math.pi
+    N = 30
+    tol = 1e-10
+    assert abs(calculator.cos(x,N) - math.cos(x)) < tol
