45bb868f5c1b21bef1e07c5bab917bdfb45d6381
Kristin Eng <kristineng@eduroam-193-157-164-15.wlan.uio.no>
2020-09-02 10:44:51 +0200

    Gjorde en liten endring på factorial

--- calculator.py
+++ calculator.py
@@ -1,22 +1,6 @@
 def add(x,y):
     return (x+y)
 
-"""
-def factorial(n):
-    if n < 0:
-        return 0
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        i = 2
-        last_product = 1
-        while i <= n:
-            product = i * last_product
-            last_product = product
-            i += 1
-        return product
-"""
-
 def factorial(n):
     if n < 0:
         return 0
